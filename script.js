fd7796d42661314e3a5b49ac06cbf6fdb510fda2
Tejas <tejas9075674655@gmail.com>
2024-03-01 21:07:49 +0530

    shape-color project

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ const shape = document.getElementById('shape');
 const changeColorBtn = document.getElementById('changeColorBtn');
 const changeShapeBtn = document.getElementById('changeShapeBtn');
 
-
 const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33C1', '#33C1FF', '#C1FF33', '#FF3333', '#33FF33', '#3333FF', '#FFFF33'];
 
 
-const shapes = ['triangle', 'square', 'circle', 'rectangle', 'diamond'];
+const shapes = ['circle', 'square', 'rectangle'];
 
+let currentShapeIndex = 0;
 
 function getRandomColor() {
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function getRandomShape() {
-    return shapes[Math.floor(Math.random() * shapes.length)];
+
+function changeShape() {
+    shape.style.display = 'none'; 
+    const currentShape = shapes[currentShapeIndex];
+    shape.className = `shape ${currentShape}`;
+    shape.style.display = 'block';
+    currentShapeIndex = (currentShapeIndex + 1) % shapes.length;
 }
 
 changeColorBtn.addEventListener('click', () => {
@@ -22,6 +27,8 @@ changeColorBtn.addEventListener('click', () => {
 });
 
 changeShapeBtn.addEventListener('click', () => {
-    const randomShape = getRandomShape();
-    shape.className = `shape ${randomShape}`;
+    changeShape();
 });
+
+
+changeShape();
